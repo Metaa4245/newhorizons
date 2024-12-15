341854634299dbbfa3e15aa8174202631ecdeb54
Metaa4245 <cappymeta@gmail.com>
2024-12-15 17:10:06 +0200

    switch out logger and add url parsing

--- main.go
+++ main.go
@@ -3,37 +3,48 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
-	"log"
+	"log/slog"
 	"net"
+	"net/url"
 )
 
 func handleConn(c net.Conn) {
 	r := bufio.NewReader(c)
 
-	s, err := r.ReadString('\n')
+	str, err := r.ReadString('\n')
 	if err != nil {
-		log.Fatal(err)
+		slog.Error(err.Error())
+		return
 	}
-	log.Println(s)
+
+	u, err := url.Parse(str)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+
+	println(u.Path)
 }
 
 func main() {
 	cert, err := tls.LoadX509KeyPair("cert.pem", "cert.key")
 	if err != nil {
-		log.Fatal(err)
+		slog.Error(err.Error())
+		return
 	}
 	config := &tls.Config{Certificates: []tls.Certificate{cert}}
 
 	l, err := tls.Listen("tcp", ":1965", config)
 	if err != nil {
-		log.Fatal(err)
+		slog.Error(err.Error())
+		return
 	}
 	defer l.Close()
 
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			log.Fatal(err)
+			continue
 		}
 
 		go handleConn(conn)
